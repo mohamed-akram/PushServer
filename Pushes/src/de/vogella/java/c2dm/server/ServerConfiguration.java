dfa953be76920758e3145512fa27863ec7ca7696
akram <mohamed.akram@rediffmail.com>
2012-02-23 19:04:51 +0530

    Hello

---
+++ Pushes/src/de/vogella/java/c2dm/server/ServerConfiguration.java
@@ -0,0 +1,8 @@
+package de.vogella.java.c2dm.server;
+
+public class ServerConfiguration {
+	public static final String AUTHENTICATION_TOKEN = "your_token";
+	public static final String REGISTRATION_ID = "registration_id_of_your_device";
+
+}
+
---
+++ Pushes/src/de/vogella/java/c2dm/server/secret/SecureStorage.java
@@ -0,0 +1,7 @@
+package de.vogella.java.c2dm.server.secret;
+
+public class SecureStorage {
+	public static final String USER = "your_registeredUser";
+	public static final String PASSWORD = "your_password";
+}
+
